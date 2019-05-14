11c067d3ab43db8a57054ffb30f864b58ebe8065
Talman Aarne J <aarne.talman@helsinki.fi>
2019-05-14 15:44:13 +0300

    LSTM with word embeddings

--- download_embeddings.sh
+++ download_embeddings.sh
@@ -7,9 +7,9 @@ mkdir embeddings
 echo -e "Done!"
 
 # Download and unzip GloVe word embeddings
-echo -e "\nDownloading and unzipping Glove 840B 300D to .vector_cache\n"
+echo -e "\nDownloading and unzipping Glove 840B 300D to embeddings\n"
 wget http://nlp.stanford.edu/data/glove.840B.300d.zip
-unzip -a glove.840B.300d.zip -d .vector_cache/
+unzip -a glove.840B.300d.zip -d embeddings/
 rm -f glove.840B.300d.zip
 echo -e "\nDone!"
 
