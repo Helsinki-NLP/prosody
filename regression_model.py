7a6455339466e1341c1bf87e10e27eb49f7c8788
Hande Celikkanat <hande.celikkanat@gmail.com>
2019-05-14 11:49:32 +0300

    regression: uncased-bert, added scripts/ folder and some go-to scripts

--- regression_model.py
+++ regression_model.py
@@ -7,7 +7,7 @@ from pytorch_pretrained_bert import BertModel
 class RegressionModel(nn.Module):
     def __init__(self, device, config):
         super().__init__()
-        self.bert = BertModel.from_pretrained('bert-base-cased')
+        self.bert = BertModel.from_pretrained('bert-base-uncased')
 
         self.fc = nn.Linear(768, 1)
         self.device = device
