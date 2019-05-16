3f1601c1ef250be93cd3fcd224b84ad7ca21ea90
Talman Aarne J <aarne.talman@helsinki.fi>
2019-05-16 21:23:58 +0300

    fixed datasets

--- prosody_dataset.py
+++ prosody_dataset.py
@@ -60,6 +60,7 @@ def load_dataset(config):
     splits = dict()
     words = []
     all_sents = []
+
     for split in [config.train_set, 'dev', 'test']:
         tagged_sents = []
         with open(config.datadir+'/'+split+'.txt') as f:
@@ -69,8 +70,6 @@ def load_dataset(config):
                 sent = []
                 for line in lines:
                     split_line = line.split('\t')
-                    if len(split_line) < 2:
-                        print("here")
                     sent.append((split_line[0], split_line[1]))
                     words.append(split_line[0])
                 tagged_sents.append(sent)
